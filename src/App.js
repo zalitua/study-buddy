0c25fdd4f967a603cafc57bd98349a2b07983d4a
Zali <qwf5520@autuni.ac.nz>
2024-08-25 13:07:29 +1200

    redo login
    use login from: "react-firebase-chat"
    (avatars commented out as may be usefull in the future.
    register adds the user to auth and creates a user doc in the users collection in the database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 //import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignUp from "./components/signup/signup";
+import Login from "./components/login/Login";
 //import Dashboard from "./components/dashboard/dashboard";
 
 function App() {
   return (
     <div>
-      <SignUp />
+      <Login />
     </div>
   );
 }
