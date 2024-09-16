e48a40a55ef488cbcfb2a98adae9a901c23283f2
kny725979 <kny725979@gmail.com>
2024-09-16 18:56:37 +1200

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
+// TODO: Add SDKs for Firebase products tha]t you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
