3852647695a976609322bc41fb99fa04f6ebe07c
Zali <qwf5520@autuni.ac.nz>
2024-10-11 14:20:37 +1300

    Update App.js
    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { UserAuthContextProvider } from "./context/userAuthContext";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
 import { ProfileProvider } from "./context/ProfileContext";
 
 import Sidebar from "./components/sidebar/Sidebar";
