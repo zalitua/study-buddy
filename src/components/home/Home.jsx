d565a55430d7fb28c26da6815f5d5ce4cfd2c062
Zali <qwf5520@autuni.ac.nz>
2024-10-16 21:23:09 +1300

    Configuring Layout and Design
    Assigning text and color globally
    setting look and feel of container and content panes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -32,13 +32,13 @@ const Home = () => {
       <div>
         <h1>StudyBuddy</h1>
         <h2>Welcome to StudyBuddy!</h2>
-        <p>
+        <div className="container-white">
           StudyBuddy is an interacitve environment designed to make organizing
           and managing your group projects easy and fun. By members being
           rewarded and recognized while using the app, they are encouraged to
           engage in a friendly, competitve and motivational space that will aid
           in increasing your teams productivity!
-        </p>
+        </div>
       </div>
       <div>
         <Button variant="primary" onClick={handleNavLogin}>
