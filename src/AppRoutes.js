26b7277b5169fc0d3d8c4879557d69927ba69b8c
kny725979 <kny725979@gmail.com>
2024-10-18 03:23:21 +1300

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -15,6 +15,7 @@ import Home from "./components/home/Home";
 import Tasks from "./components/tasks/Tasks";
 import Group from "./components/group/Group";
 import CustomAvatar from "./components/avatar/CustomAvatar";
+import Layout from "./components/layout/Layout";
 
 import ForumHome from "./components/forum/ForumHome";
 import ForumPage from "./components/forum/forumPages/ForumPages";
@@ -26,111 +27,112 @@ import GroupPage from "./components/group/groupPage/GroupPage";
 function AppRoutes() {
   return (
     <Routes>
-      {/* each route is defined */}
-      <Route path="/" element={<Home />} />
-      {/* routes that require a user to be logged in are protected */}
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route
-        path="/chat/:groupId/:chatId"
-        element={
-          <ProtectedRoute>
-            <Chat />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/group"
-        element={
-          <ProtectedRoute>
-            <Group />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/leaderboard"
-        element={
-          <ProtectedRoute>
-            <Leaderboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/profileForm"
-        element={
-          <ProtectedRoute>
-            <ProfileForm />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/profilePage"
-        element={
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/profilePage/:userId"
-        element={
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/userList" element={<UserList />} />
-      <Route
-        path="/calendar"
-        element={
-          <ProtectedRoute>
-            <Calendar />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/customAvatar" element={<CustomAvatar />} />
-      <Route
-        path="/tasks"
-        element={
-          <ProtectedRoute>
-            <Tasks />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/forumHome"
-        element={
-          <ProtectedRoute>
-            <ForumHome />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/forumHome/:forumId"
-        element={
-          <ProtectedRoute>
-            <ForumPage />
-          </ProtectedRoute>
-        }
-      />
-      {/*nav to a specific forum*/}
-      <Route
-        path="/group/groupPage/:groupId"
-        element={
-          <ProtectedRoute>
-            <GroupPage />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/" element={<Layout />}>
+        {/* each route is defined */}
+        <Route path="/" element={<Home />} />
+        {/* routes that require a user to be logged in are protected */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/chat/:groupId/:chatId"
+          element={
+            <ProtectedRoute>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/group"
+          element={
+            <ProtectedRoute>
+              <Group />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/leaderboard"
+          element={
+            <ProtectedRoute>
+              <Leaderboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profileForm"
+          element={
+            <ProtectedRoute>
+              <ProfileForm />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profilePage"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profilePage/:userId"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/userList" element={<UserList />} />
+        <Route
+          path="/calendar"
+          element={
+            <ProtectedRoute>
+              <Calendar />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/customAvatar" element={<CustomAvatar />} />
+        <Route
+          path="/tasks"
+          element={
+            <ProtectedRoute>
+              <Tasks />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/forumHome"
+          element={
+            <ProtectedRoute>
+              <ForumHome />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/forumHome/:forumId"
+          element={
+            <ProtectedRoute>
+              <ForumPage />
+            </ProtectedRoute>
+          }
+        />
 
+        <Route
+          path="/group/groupPage/:groupId"
+          element={
+            <ProtectedRoute>
+              <GroupPage />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
     </Routes>
   );
 }
