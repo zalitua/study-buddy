7437544602936cbbb782ad67d11eccbe99822b59
kny725979 <kny725979@gmail.com>
2024-10-22 04:29:12 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { UserAuthContextProvider } from "./context/userAuthContext";
 import { ProfileProvider } from "./context/ProfileContext";
 
-import Sidebar from "./components/sidebar/Sidebar";
-import AppRoutes from "./AppRoutes";
+import AppRoutes from "./components/approutes/AppRoutes";
 
 import "./App.css";
 
